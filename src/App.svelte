427c7e1c1e89c89b29504f442950461ef5aa596e
GikDev <m.m.bhrm85@gmail.com>
2022-04-11 15:14:10 +0430

    added startup files

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+<style lang="stylus">
+ 
+</style>
+<button class="bg-red-400">hilleo world</button>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/*.{svelte,js,ts}',
+    '.src/**/*.{svelte,js,ts}',
+    './src/**/**/*.{svelte,js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+}
