52d20ce173d0aa03c4e273b48da151a6d838dac9
GikDev <m.m.bhrm85@gmail.com>
2022-04-28 10:28:41 +0430

    remove responsiveness

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Nav from './lib/Nav.svelte'
+  import Hero from './lib/Hero.svelte'
 </script>
 
-<Nav />
+<Hero />
--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,5 +1,19 @@
 <script lang='ts'>
-  import Nav from '../Nav/index.svelte'
+  import Nav from './Nav.svelte'
 </script>
 
-<Nav />
+
+<div class="hero min-h-screen bg-info text-neutral-focus">
+  <div class="hero-content flex-col">
+    <Nav />
+    <div class="flex-row-reverse">
+      s
+      <img alt='random' src="https://picsum.photos/260/400" class="max-w-sm rounded-lg shadow-2xl" />
+      <div>
+        <h1 class="text-5xl font-bold">Box Office News!</h1>
+        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</div>
