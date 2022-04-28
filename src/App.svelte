49dc032be99f7e42150055b026da5fa9810afa93
GikDev <m.m.bhrm85@gmail.com>
2022-04-28 14:02:24 +0430

    started to make feature cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Hero from './lib/Hero.svelte'
+  import Features from './lib/Features.svelte';
 </script>
 
 <Hero />
+<Features />
---
+++ src/lib/Features.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Feature from "./Feature.svelte";
+  const features = [
+    {
+      title: 'Easy to use', 
+      logo: '',
+      desc: 'So easy, just need to learn it!',
+      id: 0
+    },
+    {
+      title: 'Best Features', 
+      logo: '',
+      desc: 'We have a lot of features, even more than you can think of.',
+      id: 1
+    },
+    {
+      title: 'Loved by users', 
+      logo: '',
+      desc: 'Almost every one that used it, loved it',
+      id: 2
+    },
+  ]
+</script>
+
+<div class="flex justify-around py-32">
+  {#each features as {title, logo, desc, id}}
+    <Feature {title} {logo} {desc} {id} />
+  {/each}
+</div>
